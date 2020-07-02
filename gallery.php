00d1f98c767ed5d4b8fe9f59c1548b0bc712db2a
Saharasultana <saharaiit07@gmail.com>
2020-07-02 17:29:30 +0600

    gallery

--- gallery.php
+++ gallery.php
@@ -9,35 +9,49 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-    <link href="js/simple-lightbox.min.css" rel="stylesheet" />
+    <link href="js/nanogallery2.min.css" rel="stylesheet" />
+    <link href="js/nanogallery2.woff.min.css" rel="stylesheet" />
     <title>Gallery</title>
 </head>
 <body>
-<div class="gallery"> 
+<a href="image/1.jpg" data-ngThumb="thumb-1.jpg"></a>
+<a href="image/2.jpg" data-ngThumb="thumb-2.jpg"></a>
+<a href="image/3.jpg" data-ngThumb="thumb-3.jpg"></a>
 
-<a href="1.jpg" class="big" rel="rel1">
-  <img src="thumb1.jpg" alt="" title="Image 1">
-</a>
-
-<a href="2.jpg" class="big" rel="rel1">
-  <img src="thumb2.jpg" alt="" title="Image 2">
-</a>
-
-<a href="3.jpg" class="big" rel="rel2">
-  <img src="thumb3.jpg" alt="" title="Image 3">
-</a>
-
-<a href="4.jpg" class="big" rel="rel2">
-  <img src="thumb4.jpg" alt="" title="Image 4">
-</a>
-
+<div id="nanogallery2" data-nanogallery2='{OPTIONS HERE}' >
+  <a href="1.jpg" data-ngThumb="thumb-1.jpg"></a>
+  <a href="2.jpg" data-ngThumb="thumb-2.jpg"></a>
+  <a href="3.jpg" data-ngThumb="thumb-3.jpg"></a>
+  ...
 </div>
 
+<!-- <div id="nanogallery2">Loading...</div> -->
 
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="js/simple-lightbox.min.js"></script>
 <!-- As A jQuery Plugin -->
-<script src="js/jquery.min.js"></script>
-<script src="js/simple-lightbox.jquery.min.js"></script>
+<script src="js/jquery.nanogallery2.core.min.js"></script>
+<script src="js/jquery.nanogallery2.data_flickr.min.js"></script>
+<script src="js/jquery.nanogallery2.data_google3.min.js"></script>
+<script src="js/jquery.nanogallery2.data_nano_photos_provider2.min.js"></script>
+<script src="js/jquery.nanogallery2.js"></script>
+<script src="js/jquery.nanogallery2.min.js"></script>
+
+
+<!-- <script> --
+$("#nanogallery2").nanogallery2( {
+  items: [
+    { src: '1.jpg', srct: 'thumb-1.jpg', title: 'Alt 1' },
+    { src: '2.jpg', srct: 'thumb-2.jpg', title: 'Alt 2' },
+    { src: '3.jpg', srct: 'thumb-3.jpg', title: 'Alt 3' }
+  ]
+});
+</script>
+<!--  -->
+
 
     
 </body>
