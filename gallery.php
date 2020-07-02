3169a69ea11b948b6cdf8b75e0ffb94c21a446a2
TheElric <rafimahafid@gmail.com>
2020-07-02 17:37:26 +0600

    gallery fix -Rafi

--- gallery.php
+++ gallery.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,39 +10,40 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-    <link href="js/nanogallery2.min.css" rel="stylesheet" />
-    <link href="js/nanogallery2.woff.min.css" rel="stylesheet" />
+    <link href="css/nanogallery2.min.css" rel="stylesheet" />
+    <link href="css/nanogallery2.woff.min.css" rel="stylesheet" />
     <title>Gallery</title>
 </head>
+
 <body>
-<a href="image/1.jpg" data-ngThumb="thumb-1.jpg"></a>
-<a href="image/2.jpg" data-ngThumb="thumb-2.jpg"></a>
-<a href="image/3.jpg" data-ngThumb="thumb-3.jpg"></a>
+    <a href="image/1.jpg" data-ngThumb="image/1.jpg"></a>
+    <a href="image/2.jpg" data-ngThumb="image/2.jpg"></a>
+    <a href="image/3.jpg" data-ngThumb="image/3.jpg"></a>
 
-<div id="nanogallery2" data-nanogallery2='{OPTIONS HERE}' >
-  <a href="1.jpg" data-ngThumb="thumb-1.jpg"></a>
-  <a href="2.jpg" data-ngThumb="thumb-2.jpg"></a>
-  <a href="3.jpg" data-ngThumb="thumb-3.jpg"></a>
-  ...
-</div>
+    <div id="nanogallery2" data-nanogallery2='{OPTIONS HERE}'>
+        <a href="image/1.jpg" data-ngThumb="image/1.jpg"></a>
+        <a href="image/2.jpg" data-ngThumb="image/2.jpg"></a>
+        <a href="image/3.jpg" data-ngThumb="image/3.jpg"></a>
 
-<!-- <div id="nanogallery2">Loading...</div> -->
+    </div>
 
-
+    <!-- <div id="nanogallery2">Loading...</div> -->
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="js/simple-lightbox.min.js"></script>
-<!-- As A jQuery Plugin -->
-<script src="js/jquery.nanogallery2.core.min.js"></script>
-<script src="js/jquery.nanogallery2.data_flickr.min.js"></script>
-<script src="js/jquery.nanogallery2.data_google3.min.js"></script>
-<script src="js/jquery.nanogallery2.data_nano_photos_provider2.min.js"></script>
-<script src="js/jquery.nanogallery2.js"></script>
-<script src="js/jquery.nanogallery2.min.js"></script>
 
 
-<!-- <script> --
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/simple-lightbox.min.js"></script>
+    <!-- As A jQuery Plugin -->
+    <script src="js/jquery.nanogallery2.core.min.js"></script>
+    <script src="js/jquery.nanogallery2.data_flickr.min.js"></script>
+    <script src="js/jquery.nanogallery2.data_google3.min.js"></script>
+    <script src="js/jquery.nanogallery2.data_nano_photos_provider2.min.js"></script>
+    <script src="js/jquery.nanogallery2.js"></script>
+    <script src="js/jquery.nanogallery2.min.js"></script>
+
+
+    <!-- <script> --
 $("#nanogallery2").nanogallery2( {
   items: [
     { src: '1.jpg', srct: 'thumb-1.jpg', title: 'Alt 1' },
@@ -53,7 +55,7 @@ $("#nanogallery2").nanogallery2( {
 <!--  -->
 
 
-    
+
 </body>
 
 
