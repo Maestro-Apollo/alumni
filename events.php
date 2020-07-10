3aad6e654e295bf266cc1dca551a0438519f70b4
Saharasultana <saharaiit07@gmail.com>
2020-07-11 01:07:18 +0600

    events

---
+++ events.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/gallery.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" href="css/events.css">
+    <title>Events</title>
+</head>
+
+<body>
+
+
+    <div class="right">
+       <h3 class="header">Event Finder</h3>
+       <hr>
+       <div class="searchbar">
+         <form action="/action_page.php">
+            <input type="text" placeholder="Search events here...." name="search">
+         </form>
+       </div>
+       <h4 class="news">NEWS</h4>
+       <hr>
+       <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
+       <br>
+       <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
+       <br>
+       <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
+
+       <h4 class="archives">ARCHIVES</h4>
+       <hr>
+       <a href="#">December 2019</a>
+       <br>
+       <a href="#">July 2019</a>
+       <br>
+       <a href="#">March 2019</a>
+       <br>
+       <a href="#">January 2019</a>
+       
+
+    </div>
+    <div class="left">
+      <img src="image/image5.jpg" alt="image">
+      
+        <div class="info">
+           <h4>NEW TOUR</h4>
+           <br>
+           <p class="activity"> Test Activity</p>
+        </div>
+
+    </div>
+    <button type="submit" name="submit" class="btn btn-primary">ADD EVENT</button>
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
