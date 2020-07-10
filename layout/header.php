17cd6d39cc4ea1691391c366ae1e1d2c272bcf76
TheElric <rafimahafid@gmail.com>
2020-07-10 21:25:45 +0600

    fix

--- layout/header.php
+++ layout/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-light">
     <div class="container">
-        <a class="navbar-brand" href="index.php"><img src="image/alumni.png" alt=""></a>
+        <a class="navbar-brand font-weight-bold" href="index.php"><img src="image/alumni.png" alt="">IIT-JU</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
