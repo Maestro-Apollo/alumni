8c9074cd5acb9dbcdaa0d2b53e865342e3b40a01
TheElric <rafimahafid@gmail.com>
2020-07-11 02:24:18 +0600

    rafi

--- events.php
+++ events.php
@@ -15,67 +15,68 @@
 
 <body>
 
-<div class="container">
-    <div class="row">
-    
-        <div class="col-md-8 left div">
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="image/image5.jpg" alt="image">
-                    <p>Event Activity</p>
-                    <p>Event Activity</p>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-8 left-div">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="image/image5.jpg" alt="image">
+                        <p>Event Activity</p>
+                        <p>Event Activity</p>
+                    </div>
+                    <div class="col-md-6">
+                        <h5>NEW TOUR</h5>
+                        <p>Test Activity</p>
+                        <p>Date</p>
+                        <p>Time</p>
+                        <p>Type</p>
+                        <p>Duration</p>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <h5>NEW TOUR</h5>
-                    <p>Test  Activity</p>
-                    <p>Date</p>
-                    <p>Time</p>
-                    <p>Type</p>
-                    <p>Duration</p>
-                </div>
+
             </div>
 
+            <div class="col-md-4 right-div">
+
+                <h3 class="header">Event Finder</h3>
+                <hr>
+                <div class="searchbar">
+                    <form action="/action_page.php">
+                        <input type="text" placeholder="Search events here...." name="search">
+                    </form>
+                </div>
+                <h4 class="news">NEWS</h4>
+                <hr>
+                <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
+                <br>
+                <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
+                <br>
+                <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
+
+                <h4 class="archives">ARCHIVES</h4>
+                <hr>
+                <a href="#">December 2019</a>
+                <br>
+                <a href="#">July 2019</a>
+                <br>
+                <a href="#">March 2019</a>
+                <br>
+                <a href="#">January 2019</a>
+            </div>
         </div>
 
-        <div class="col-md-4 right div">
-       
-           <h3 class="header">Event Finder</h3>
-           <hr>
-           <div class="searchbar">
-              <form action="/action_page.php">
-                 <input type="text" placeholder="Search events here...." name="search">
-              </form>
-           </div>
-           <h4 class="news">NEWS</h4>
-           <hr>
-           <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
-           <br>
-           <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
-           <br>
-           <a href="#">Seminar on ROBOTICS - Introductions And Challenges</a>
-
-           <h4 class="archives">ARCHIVES</h4>
-           <hr>
-           <a href="#">December 2019</a>
-           <br>
-           <a href="#">July 2019</a>
-           <br>
-           <a href="#">March 2019</a>
-           <br>
-           <a href="#">January 2019</a>
-        </div>
     </div>
 
-</div>
 
 
 
 
-    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
 
 </body>
+
 </html>
