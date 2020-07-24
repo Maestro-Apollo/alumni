45146fd4545172522c2fa122de213bc4474751b3
TheElric <rafimahafid@gmail.com>
2020-07-24 22:45:15 +0600

    contact

--- contact.php
+++ contact.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/gallery.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
 
 
@@ -21,19 +20,21 @@
         <?php include('layout/header.php'); ?>
     </section>
 
-    
+
     <section>
-    <h2 id="heading">Contact</h2>
-    <div class="container">
-    
-    <h2>IIT-Alumni Association,Jahangirnagar University</h2>
-    <h4>Savar, Dhaka-1209</h4>
-    <h5>Email:iitalumniju@gmail.com</h5>
-    <h5>Mobile No: 01xxx-xxxxxx</h5>
-    </div>
+        <div class="text-center">
+            <h2 id="heading">Contact</h2>
+            <div class="container p-5">
+
+                <h2>IIT-Alumni Association,Jahangirnagar University</h2>
+                <h4>Savar, Dhaka-1209</h4>
+                <h5>Email:iitalumniju@gmail.com</h5>
+                <h5>Mobile No: 01xxx-xxxxxx</h5>
+            </div>
+        </div>
     </section>
 
-   
+
 
     <section>
         <?php include('layout/footer.php'); ?>
